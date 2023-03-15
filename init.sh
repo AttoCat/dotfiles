a127b4c1d6a9d61456d0a012eb63899d830453b5
AttoCat <contact@attocat.com>
2023-03-15 17:20:48 +0900

    :sparkle: Support multi OS

--- init.sh
+++ init.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 if [ $(uname) = Darwin ]; then
-    if ! type -t brew ; then
-        echo "---Install and update Homebrew---"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo "---Installed!!---"
-    else
-        echo "---Homebrew is already installed, update...---"
-    fi
-    brew update > /dev/null
-    echo "---Install some packages with HomeBrew---"
-    brew bundle > /dev/null 2>&1
-    echo "---Installed!---"
+    source ./mac/mac-init.sh
 fi
 
 fish -c "fisher update"
+echo "---Set fish as login shell---"
 chsh -s `which fish`
 fish
---
+++ mac/mac-init.sh
@@ -0,0 +1,15 @@
+if ! type -t brew ; then
+    echo "---Install and update Homebrew---"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "---Installed!!---"
+else
+    echo "---Homebrew is already installed, update...---"
+fi
+brew update > /dev/null
+echo "---Install some packages with HomeBrew---"
+brew bundle > /dev/null 2>&1
+echo "---Installed!---"
+echo "---Restore config of software---"
+defaults import com.codefalling.MacGesture ./mac/macgesture-backup.plist
+defaults import org.p0deje.Maccy ./mac/macgesture-backup.plist
+echo "---Restored!---"
